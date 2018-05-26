1d0f67d7aac7626f6a8838681e3e9657ae7dccd1
fesaza <fesaza@gmail.com>
2018-05-26 14:48:37 -0500

    refactor createFeature

--- src/containers/feature/createComponentFeature.js
+++ src/containers/feature/createComponentFeature.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import * as CompomentTypes from '../../models/ComponentTypes';
+import * as ComponentTypes from '../../models/ComponentTypes';
 import List from '../Lists/MembersListPage';
+
+function getComponent(component) {
+  if (component.component) {
+    return getComponent(component.component);
+  }
+
+  switch (component.type) {
+    case ComponentTypes.GridList:
+    case ComponentTypes.CardList:
+      return (<List configuration={component} />);
+    default:
+      return (<div>Loaded but error: Component maybe doesn't have the type property</div>);
+  }
+}
 
 const createComponentFeature = configuration => () => {
   if (!configuration || configuration.isFetching) {
     return (<div>Loading configurations and components...</div>);
   }
-  const { component } = configuration;
-  // if (component) {
-  //   return createComponentFeature(component);
-  // }
-
-  if (component.type === CompomentTypes.GridList || component.type === CompomentTypes.CardList) {
-    return (<List configuration={component} />);
-  }
-  return (<div>Loaded</div>);
+  return getComponent(configuration);
 };
 
 export default createComponentFeature;
